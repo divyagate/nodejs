94e0d82941d9825d3b65454f71ca7f7da44e9ef0
divyagate <gatedivya123@gmail.com>
2020-12-04 15:23:02 -0500

    Delete connection.js
    removed file

--- connection.js
+++
@@ -1,7 +0,0 @@
-
-const http= require("http");
-console.log("Running on http://localhost:3000");
-http.createServer(function (req, res) {
-    res.write("<h1>Hello World</h1>");
-    res.end();
-  }).listen(3000);
